fd3ba2342c87d1ef2e974ef55654c9b83ba88b77
Sam Joseph <tansaku@gmail.com>
2014-06-04 17:06:33 +0100

    all the different cell states

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Faezrah,
 James K,
 James W,
 Nicolai,
-Sasha
+Sasha\
+
+https://www.evernote.com/shard/s329/sh/3ed441d9-87a6-4f8a-aea4-17b0f5e6ea80/921421801f36aef5f91f8bf6b906cba6
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -7,30 +7,41 @@ describe Grid do
 	context 'starts with' do
 	
 		it 'initializes with at least one row' do
-			expect(grid.board.length > 0).to eq(true)
+			expect(grid.board.length).to be > 0
 		end
 
 		it 'initializes every row having 10 cells' do
-			expect(grid.board.all? {|row| row.length == 10}).to eq(true)
+			grid.board.each do |row|
+				expect(row.length).to eq 10
+			end
+			#expect(grid.board.all? {|row| row.length == 10}).to eq(true)
 		end
 
 	end
 	context 'cells can have different states' do
 
 		it 'can be non-hit non-ship' do
-			expect(grid.board[0][9]).to eq []
+			expect(grid.board[0][9]).to eq nil
 		end	
 
 		it 'can be hit non-ship' do
 			grid.receive_shot(0,9)
-			expect(grid.board[0][9]).to eq [:M]
+			expect(grid.board[0][9]).to eq :M
 		end
 
 		it 'can be non-hit ship' do
 			ship = Ship.new(2)
-			grid.place_ship(1,2)
+			grid.place_ship(1,2, ship)
 			expect(grid.board[1][2]).to eq ship
 		end
+
+		it 'can be a hit ship' do
+			ship = Ship.new(2)
+			grid.place_ship(1,2, ship)
+			grid.receive_shot(1,2)
+			expect(grid.board[1][2]).to eq :H
+		end
+
 	end
 end
 
